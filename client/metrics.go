78cf12cc022629f9ca11467f5524487591398b8e
Karsa Zoltán <karsa@joker.cloud.bme.hu>
2022-09-30 18:04:45 +0200

    add metrics

--- client/metrics.go
+++ client/metrics.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 )
 
-func (c *Client) GetMetric(target string, deltatime_h int) ([]Metric, error) {
+func (c *Client) GetMetrics(target string, deltatime_h int) ([]Metric, error) {
 	body, err := c.httpRequest(fmt.Sprintf(".usage2?target%s&time=%vh", target, deltatime_h), "GET", bytes.Buffer{}, 200)
 	if err != nil {
 		return nil, err
@@ -20,13 +20,13 @@ func (c *Client) GetMetric(target string, deltatime_h int) ([]Metric, error) {
 }
 
 func (c *Client) GetAVGCPU(deltatime_h int) ([]Metric, error) {
-	return c.GetMetric("avg(circle.bm*.cpu.percent)", deltatime_h)
+	return c.GetMetrics("avg(circle.*.cpu.percent)", deltatime_h)
 }
 
 func (c *Client) GetAVGMem(deltatime_h int) ([]Metric, error) {
-	return c.GetMetric("avg(circle.bm*.memory.usage)", deltatime_h)
+	return c.GetMetrics("avg(circle.*.memory.usage)", deltatime_h)
 }
 
 func (c *Client) GetSumVM(deltatime_h int) ([]Metric, error) {
-	return c.GetMetric("sum(circle.bm*.vmcount)", deltatime_h)
+	return c.GetMetrics("sum(circle.*.vmcount)", deltatime_h)
 }
