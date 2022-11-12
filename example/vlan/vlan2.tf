02c5ad5d12894ee4eb3f63b12bb8d880e015229b
Karsa Zoltán <karsa@joker.cloud.bme.hu>
2022-11-12 18:28:22 +0100

    vlan2 ex

---
+++ example/vlan/vlan2.tf
@@ -0,0 +1,20 @@
+variable "users" {
+  type = list(string)
+  default = [ "Alice", "Bob" ]
+}
+data "circle3_template" "meres" {
+  name = "meres-sablon"
+}
+data "circle3_user" "user" {
+  for_each = toset(var.users)
+  name = "${each.key}"
+}
+resource "circle3_vm" "for_each_users" {
+  for_each = toset(var.users)
+  name = "vm pool ${each.key}"
+  from_template = data.circle3_template.meres.id
+  owner = data.circle3_user.user[each.key].id   
+}
+
+
+
